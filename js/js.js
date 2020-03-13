977e04d8763f549950b610feb8a47d934c6bbf8e
kiliany.lcssc@eduge.ch <LECOSSECK@lab.ecinf.ch>
2020-03-13 16:20:02 +0100

    Fixed Jump Bug

--- js/js.js
+++ js/js.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
+var debug1 = document.getElementById("debug1");
+var debug2 = document.getElementById("debug2");
 
 var framesPerSecond = 60;
 
@@ -26,6 +28,10 @@ function Rectangle(x = 0, y = 0, w = 0, h = 0, color = "white")
   this.moveLeft = false;
   this.moveRight = false;
   this.isGrounded = false;
+  this.Left = this.x;
+  this.Right = this.x + this.width;
+  this.Top = this.y;
+  this.Bottom = this.y + this.height;
   this.sprite = new Image();
 
   // Methods
@@ -55,7 +61,6 @@ function ManageKeyDown(e)
     if (player.isGrounded)
     {
       gravity = player.gravity;
-      player.isGrounded = false;
     }
   }
   // "A" Key
@@ -95,6 +100,9 @@ function ManageKeyUp(e)
 // Player object
 var player = new Rectangle(0, canvas.height - 50, 50, 50, "lightgreen");
 
+// Obstacle object
+var obstacle = new Rectangle(canvas.width / 4, canvas.height - 50, 500, 50, "green");
+
 // Background object
 var background = new Rectangle(0, 0, canvas.width, canvas.height);
 background.sprite.src = "img/background.jpg";
@@ -105,7 +113,19 @@ var gravity = player.gravity;
 // The game loop
 function Update()
 {
-  if (player.moveLeft)
+  // Recalculates the coordinates
+  player.Left = player.x;
+  player.Right = player.x + player.width;
+  player.Top = player.y;
+  player.Bottom = player.y + player.height;
+
+  obstacle.Left = obstacle.x;
+  obstacle.Right = obstacle.x + obstacle.width;
+  obstacle.Top = obstacle.y;
+  obstacle.Bottom = obstacle.y + obstacle.height;
+
+  // Movement + Left Collision
+  if (player.moveLeft && player.Left > obstacle.Right)
   {
     player.Move(-player.speed);
   }
@@ -124,13 +144,13 @@ function Update()
   if (jumpIsDown && player.isGrounded)
   {
     player.isGrounded = false;
+    jumpIsDown = false;
   }
 
   if (!player.isGrounded)
   {
     player.y -= gravity;
     gravity--;
-    console.log(gravity);
   }
 }
 
@@ -140,7 +160,12 @@ function Render()
   // Draws the background
   ctx.drawImage(background.sprite, background.x, background.y, background.width, background.height);
 
+  DrawRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height, obstacle.color);
+
   DrawRect(player.x, player.y, player.width, player.height, player.color);
+
+  debug1.innerHTML = "Player Left : " + player.Left;
+  debug2.innerHTML = "Obstacle Right : " + obstacle.Right;
 }
 
 setInterval(Update, 1000 / framesPerSecond);
